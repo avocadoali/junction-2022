5037e899aaea65a5ed09fb5fe6346b22a5f6095f
AlisArch <alfrednguyen02@gmail.com>
2022-11-06 03:08:06 +0200

    history and contents

--- web_app/app/main.py
+++ web_app/app/main.py
@@ -20,12 +20,8 @@ def home_view():
 @app.route('/cardpage', methods=['GET', 'POST'])
 def cardpage():      
     session.permanent = True
-    # data = cardDemo()
-
-    data =   {'atr':     {'atr': '3B 6E 00 00 80 31 80 66 B0 84 0C 01 6E 01 83 00 90 00', 'atrFlag': False, 'atrID': ''}, 'name': 'CARD/S-E VISA DB ONL ACP  ', 'number': '43 18 71 19 91 39 94 79', 'effectiveDate': '28/11/2016', 'expirationDate': '30/11/2019', 'currency': 'Euro', 'country': 'The Republic of Finland', 'log': [
-{'amount': '0.01', 'date': '09/09/2021', 'currency': 'Euro'},
-{'amount': '0.55', 'date': '24/06/2021', 'currency': 'Euro'}, {'amount': '0.11', 'date': '24/06/2021', 'currency': 'Euro'}, {'amount': '5.55', 'date': '24/06/2021', 'currency': 'Euro'}, {'amount': '0.01', 'date': '24/06/2021', 'currency': 'Euro'}, {'amount': '0.11', 'date': '24/06/2021', 'currency': 'Euro'}, {'amount': '0.01', 'date': '24/06/2021', 'currency': 'Euro'}, {'amount': '0.06', 'date': '24/06/2021', 'currency': 'Euro'}, {'amount': '1.23', 'date': '19/02/2021', 'currency': 'Euro'}, {'amount': '30.00', 'date': '16/01/2020', 'currency': 'Euro'}]}
-
+    data = cardDemo()
+    #data = {'atr': {'atr': '3B 6D 00 00 00 73 C8 00 13 64 54 37 44 33 00 90 00', 'atrFlag': 'possible matched card', 'atrID': {'3B 6D 00 00 00 73 C8 00 13 64 54 37 44 33 00 90 00': ['Deutsche Bank MasterCard Credit Card (Bank)', 'https://www.deutsche-bank.de/pfb/content/pk-konto-und-karte-kreditkarte.html']}}, 'name': ['0x5F20: 54 45 53 54 20 43 41 52 44 2F 45 4D 56 20 42 49 4E 2D 32', 'TEST CARD/EMV BIN-2'], 'number': ['0x57: 22 23 00 00 10 01 26 46', '2223 0000 1001 2646'], 'effectiveDate': ['0x5F25: 15 11 01', '01/11/2015'], 'expirationDate': ['0x5F24: 19 12 31', '31/12/2019'], 'currency': ['0x9F42: 0840', 'ISO currency code: 0840'], 'country': ['0x5F28: 0840', 'ISO country code: 0840'], 'log': [{'amount': '1.23', 'date': '01/11/2022', 'currency': 'Euro'}, {'amount': '1.23', 'date': '01/11/2022', 'currency': 'Euro'}, {'amount': '1.23', 'date': '01/11/2022', 'currency': 'Euro'}, {'amount': '1.23', 'date': '01/11/2022', 'currency': 'Euro'}, {'amount': '0.01', 'date': '27/10/2022', 'currency': 'Euro'}, {'amount': '0.01', 'date': '27/10/2022', 'currency': 'Euro'}, {'amount': '70.00', 'date': '06/10/2022', 'currency': 'Euro'}, {'amount': '32.01', 'date': '06/10/2022', 'currency': 'Euro'}, {'amount': '12.81', 'date': '06/10/2022', 'currency': 'Euro'}, {'amount': '0.06', 'date': '06/10/2022', 'currency': 'Euro'}]}
 
     session["data"] = data
     card_logo = ""
@@ -43,5 +39,21 @@ def cardpage():
 def log():
     
     data = session["data"]
-    return render_template('log.html', data_list = data)
+    data_clean = {}
 
+    print(data)
+
+    for x in data:
+        if x != "atr" and x != "log":
+            data_clean[x.upper()]= data[x]
+
+    data_history = data["log"]
+
+    if not data_history:
+        data_history["a"] = "empty"
+
+    data_atr = data["atr"]
+
+
+
+    return render_template('log.html', data_list = data_clean, data_hist = data_history)
