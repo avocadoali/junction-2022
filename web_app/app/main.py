2de8fe919ced2d250bdfc114d468ccf16551a115
AlisArch <alfrednguyen02@gmail.com>
2022-11-06 04:29:27 +0200

    fixed main

--- web_app/app/main.py
+++ web_app/app/main.py
@@ -24,9 +24,11 @@ def cardpage():
     #data = {'atr': {'atr': '3B 6D 00 00 00 73 C8 00 13 64 54 37 44 33 00 90 00', 'atrFlag': 'possible matched card', 'atrID': {'3B 6D 00 00 00 73 C8 00 13 64 54 37 44 33 00 90 00': ['Deutsche Bank MasterCard Credit Card (Bank)', 'https://www.deutsche-bank.de/pfb/content/pk-konto-und-karte-kreditkarte.html']}}, 'name': ['0x5F20: 54 45 53 54 20 43 41 52 44 2F 45 4D 56 20 42 49 4E 2D 32', 'TEST CARD/EMV BIN-2'], 'number': ['0x57: 22 23 00 00 10 01 26 46', '2223 0000 1001 2646'], 'effectiveDate': ['0x5F25: 15 11 01', '01/11/2015'], 'expirationDate': ['0x5F24: 19 12 31', '31/12/2019'], 'currency': ['0x9F42: 0840', 'ISO currency code: 0840'], 'country': ['0x5F28: 0840', 'ISO country code: 0840'], 'log': [{'amount': '1.23', 'date': '01/11/2022', 'currency': 'Euro'}, {'amount': '1.23', 'date': '01/11/2022', 'currency': 'Euro'}, {'amount': '1.23', 'date': '01/11/2022', 'currency': 'Euro'}, {'amount': '1.23', 'date': '01/11/2022', 'currency': 'Euro'}, {'amount': '0.01', 'date': '27/10/2022', 'currency': 'Euro'}, {'amount': '0.01', 'date': '27/10/2022', 'currency': 'Euro'}, {'amount': '70.00', 'date': '06/10/2022', 'currency': 'Euro'}, {'amount': '32.01', 'date': '06/10/2022', 'currency': 'Euro'}, {'amount': '12.81', 'date': '06/10/2022', 'currency': 'Euro'}, {'amount': '0.06', 'date': '06/10/2022', 'currency': 'Euro'}]}
     data = {}
     if "data" in session:
+        print("calc data")
         data = session["data"]
     else:
-        data = cardDemo()
+        print("carDemo")
+        session["data"]= cardDemo()
 
     card_logo = "../static/assets/img/visacard.png"
 
